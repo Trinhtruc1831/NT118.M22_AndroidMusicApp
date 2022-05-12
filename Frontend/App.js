b46df129a32ad402ab2d304a9ab3df57dc3cb673
Ly  Thao <lytranthanhthao77@gmail.com>
2022-05-13 00:41:47 +0700

    add function login

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,29 +1,93 @@
-import React, {useState, useEffect} from 'react';
-import { Button, View, Text, Image } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import 'react-native-gesture-handler';
+import React, { useState, useEffect } from "react";
+import { Button, View, Text, Image } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import "react-native-gesture-handler";
 
 import Login from "./screens/Login";
 import Signup from "./screens/Signup";
 import Botnavi from "./screens/Botnavi";
 
+import firebase from "firebase/compat/app";
+
+import "firebase/compat/auth";
+
+import "firebase/compat/database";
+import "firebase/compat/firestore";
 const Stack = createStackNavigator();
 
 export default function App() {
+  // function testaddData(username, password, email, hoten) {
+  //   firebase
+  //     .database()
+  //     .ref("MEMBER/")
+  //     .push()
+  //     .set(
+  //       {
+  //         Username: username,
+  //         Password: password,
+  //         Email: email,
+  //         Hoten: hoten,
+  //       },
+  //       function (error) {
+  //         if (error) {
+  //           console.log("Lỗi");
+  //         } else {
+  //           console.log("Thành công");
+  //         }
+  //       }
+  //     );
+  // }
+
+  // const get_DATA = () => {
+  //   let array = [];
+  //   firebase
+  //     .database()
+  //     .ref("MEMBER/")
+  //     .on("value", function (snapshot) {
+  //       snapshot.forEach(function (childSnapshot) {
+  //         var childData = childSnapshot.val();
+  //         array.push({
+  //           id: childSnapshot.key,
+  //           username: childData.Username,
+  //           password: childData.Password,
+  //         });
+  //       });
+  //       console.log(array[1].username);
+  //       //setData(array);
+  //     });
+  // };
+  useEffect(() => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyBdsz9AWTFJm4ZJzQyHFJJM4U-gvsnRCFU",
+      authDomain: "rcapp-71126.firebaseapp.com",
+      projectId: "rcapp-71126",
+      storageBucket: "rcapp-71126.appspot.com",
+      messagingSenderId: "667210641239",
+      appId: "1:667210641239:web:75ebb3010d2e3f5e12d812",
+      measurementId: "G-C9Q5DRGJVK",
+    };
+    if (!firebase.apps.length) {
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      console.log("\nKết nối firebase thành công\n");
+      //testaddData("lttt", "123", "lttt0206", "Lý Thảo");
+      //get_DATA();
+    }
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
         useLegacyImplementation
         initialRouteName="Login"
         screenOptions={{ headerShown: false }}
-      >        
+      >
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="Botnavi" component={Botnavi} />
       </Stack.Navigator>
     </NavigationContainer>
-    
   );
 }
--- Frontend/screens/login.js
+++ Frontend/screens/login.js
@@ -1,55 +1,126 @@
-import { View, Text,TextInput, onChangeNumber, number, Button, ImageBackground, StyleSheet, Image } from 'react-native'
-import React, { useState } from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  onChangeNumber,
+  number,
+  Button,
+  ImageBackground,
+  StyleSheet,
+  Image,
+} from "react-native";
+import React, { useState, useEffect } from "react";
 import background from "../assets/icon/background.png";
-import Logo from "../assets/icon/Logo.png";
-import Checkbox from 'expo-checkbox';
+import Logo from "../assets/icon/logo.png";
+import Checkbox from "expo-checkbox";
+import firebase from "firebase/compat/app";
+import ConnectFirebase from "../config";
+import "firebase/compat/auth";
+
+import "firebase/compat/database";
+import "firebase/compat/firestore";
 
 export default function Login({ navigation }) {
+  const [username, setUsername] = useState(null);
+  const [password, setPass] = useState([]);
   const [isChecked, setChecked] = useState(false);
-    return (
-        <View style={styles.container}>
-          <ImageBackground source={background} resizeMode="cover" style={styles.image}>
-          <Image  source={Logo} resizeMode="contain" style={styles.logo}/>
-          <View style={styles.inputlogin}>
-            <Text style={styles.text}>
-              Tài khoản
-            </Text>
-            <TextInput
-              style={styles.input}
-              onChangeText={onChangeNumber}
-              value={number}
-              placeholder="Nhập tên tài khoản vào đây"
-              keyboardType="numeric"
-            />
-            <Text style={styles.text}>
-              Mật khẩu
-            </Text>
-            <TextInput
-              style={styles.input}
-              onChangeText={onChangeNumber}
-              value={number}
-              placeholder="Nhập mật khẩu vào đây"
-              keyboardType="numeric"
-            />
-          </View>
-          
 
-          <View style={styles.row}>
-            <Checkbox
+  function get_DATA(user, pass) {
+    ConnectFirebase();
+    let array = [];
+    firebase
+      .database()
+      .ref("MEMBER/")
+      .on("value", function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+          var childData = childSnapshot.val();
+          array.push({
+            id: childSnapshot.key,
+            Username: childData.Username,
+            Password: childData.Password,
+          });
+        });
+        //setData(array);
+      });
+
+    let i = 0;
+    while (i < array.length) {
+      if (array[i].Username == user && array[i].Password == pass) {
+        return navigation.navigate("Botnavi");
+      } else {
+        i = i + 1;
+      }
+      console.log(i);
+    }
+    if (i >= array.length) {
+      console.log("---------------------------");
+      console.log(i);
+      console.log(user);
+      console.log(pass);
+      console.log(array.length);
+      console.log(array);
+
+      alert("Lỗi");
+    }
+  }
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={background}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <Image source={Logo} resizeMode="contain" style={styles.logo} />
+        <View style={styles.inputlogin}>
+          <Text style={styles.text}>Tài khoản</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => {
+              setUsername(text);
+            }}
+            value={username}
+            value={number}
+            placeholder="Nhập tên tài khoản vào đây"
+            keyboardType="numeric"
+          />
+          <Text style={styles.text}>Mật khẩu</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => {
+              setPass(text);
+            }}
+            value={password}
+            value={number}
+            placeholder="Nhập mật khẩu vào đây"
+            keyboardType="numeric"
+          />
+        </View>
+
+        <View style={styles.row}>
+          <Checkbox
             style={styles.checkbox}
             value={isChecked}
             onValueChange={setChecked}
-            color={isChecked ? '#A88A64' : undefined}
-            />
-            <Text style={styles.labelCheckBox}>Hiện mật khẩu</Text>
-            <Text style={styles.labelFGPW} onPress={() =>
-              navigation.navigate('Signup')
-            } >Chưa có tài khoản?</Text>
-          </View>
-          <View style={styles.inputlogin}>
-
-          </View>
-          {/* <Text style={styles.text}>Đây là trang login!</Text> 
+            color={isChecked ? "#A88A64" : undefined}
+          />
+          <Text style={styles.labelCheckBox}>Hiện mật khẩu</Text>
+        </View>
+        <View style={styles.btlogin}>
+          <Button
+            title="Đăng Nhập"
+            color="#7B6242"
+            onPress={() => {
+              get_DATA(username, password);
+            }}
+          />
+        </View>
+        <Text
+          style={styles.labelFGPW}
+          onPress={() => navigation.navigate("Signup")}
+        >
+          Chưa có tài khoản?
+        </Text>
+        {/* <Text style={styles.text}>Đây là trang login!</Text> 
           <Button
             title="Đăng Nhập"
             onPress={() =>
@@ -63,82 +134,82 @@ export default function Login({ navigation }) {
             }
             style={styles.button}
           /> */}
-          </ImageBackground>
-        </View>
-      )};
-      const styles = StyleSheet.create({
-        container: {
-          flex: 1,
-        },
-        
-        inputlogin:{
-          flex: 0.5,
-          width: 380,
-          
-        },
-        row: {
-          flexDirection: "row",
-          flexWrap: "wrap",
-          width: 380,
-        },
-        image: {
-          flex: 1,
-          alignItems: 'center', 
-          justifyContent: 'center',
-        },
-        logo: {
-          flex: 1,
-          width: 380,
+      </ImageBackground>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
 
-        },
-        text: {
-          flex: 1,
-          fontStyle: 'normal',
-          fontWeight: 'bold',
-          alignSelf: 'flex-start',
-          fontSize: 15,
-          color: '#7B6242',
-          marginLeft: 20,
-          
-        },
-        input: {
-          flex: 1,
-          marginVertical: 15,
-          marginHorizontal: 18,
-          borderWidth: 2,
-          backgroundColor: '#F4ECD0',
-          borderColor: '#A88A64',
-          borderRadius: 7,
-          padding: 10,
-          
-        },
-        checkbox: {
-          marginLeft: 18,
-          height: 15,
-          width: 15,
-          backgroundColor: '#F4ECD0',
-          borderColor: '#A88A64',
-          borderRadius: 5,
-          color:'#A88A64'
-
-        },
-        labelCheckBox: {
-          alignSelf: 'flex-start',
-          marginLeft: 5,
-          fontSize: 10,
-          color: '#7B6242',
-
-        },
-        labelFGPW: {
-          alignSelf: 'flex-start',
-          marginLeft: 15,
-          fontSize: 10,
-          color: '#7B6242',
-
-        },
-        button: {
-          flex: 1,
-        },
-        
-        
-      });
+  inputlogin: {
+    flex: 0.5,
+    width: 380,
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    width: 380,
+  },
+  image: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    flex: 1,
+    width: 380,
+  },
+  text: {
+    flex: 1,
+    fontStyle: "normal",
+    fontWeight: "bold",
+    alignSelf: "flex-start",
+    fontSize: 15,
+    color: "#7B6242",
+    marginLeft: 20,
+  },
+  input: {
+    flex: 1,
+    marginVertical: 15,
+    marginHorizontal: 18,
+    borderWidth: 2,
+    backgroundColor: "#F4ECD0",
+    borderColor: "#A88A64",
+    borderRadius: 7,
+    padding: 10,
+  },
+  checkbox: {
+    marginLeft: 18,
+    height: 15,
+    width: 15,
+    backgroundColor: "#F4ECD0",
+    borderColor: "#A88A64",
+    borderRadius: 5,
+    color: "#A88A64",
+  },
+  labelCheckBox: {
+    alignSelf: "flex-start",
+    marginLeft: 5,
+    fontSize: 10,
+    color: "#7B6242",
+  },
+  labelFGPW: {
+    alignSelf: "center",
+    marginBottom: 15,
+    fontSize: 15,
+    fontStyle: "italic",
+    color: "#7B6242",
+  },
+  button: {
+    flex: 1,
+  },
+  btlogin: {
+    flex: 0.5,
+    width: 150,
+    height: 15,
+    marginTop: 10,
+    color: "#7B6242",
+  },
+});
