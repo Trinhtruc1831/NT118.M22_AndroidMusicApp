d07d9e9d9d971efda4845a114088aa400db0b543
Trinhtruc1831 <19521059@gm.uit.edu.vn>
2022-05-30 14:24:02 +0700

    addmusicart

---
+++ Content/model/data.js
@@ -0,0 +1,25 @@
+const songs=[
+    {
+        Title: "Heyahe",
+        artist:"ONE",
+        artwork: require("../assets/img/1.png"),
+        url: require("../assets/music/1.mp3"),
+        id: 1,
+        duration: 331,
+    },
+    {
+        Title: "Still with you",
+        artist:"Jungkook",
+        artwork: require("../assets/img/2.png"),
+        url: require("../assets/music/2.mp3"),
+        id: 2,
+    },
+    {
+        Title: "Hirari Hirari",
+        artist:"Hatsune Miku",
+        artwork: require("../assets/img/3.png"),
+        url: require("../assets/music/3.mp3"),
+        id: 3,
+    }
+    ];
+    export default songs;
