b2cdb02e82664c0b10ccd98e7310a2b556f9a949
Ly  Thao <lytranthanhthao77@gmail.com>
2022-06-03 16:48:57 +0700

    Home UI

--- Content/screens/Home.js
+++ Content/screens/Home.js
@@ -1,18 +1,235 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState, Component, useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SectionList,
+  SafeAreaView,
+  Image,
+  FlatList,
+  TextInput,
+} from 'react-native';
+import firebase from "firebase/compat/app";
+import ConnectFirebase from "../config";
+import "firebase/compat/auth";
 
-import { View, Text, Button } from 'react-native'
-import React from 'react'
+import "firebase/compat/database";
+import "firebase/compat/firestore";
+
+const ListItem = ({ item }) => {
+  return (
+    <View style={styles.item}>
+      <Image
+        source={{
+          uri: item.uri,
+        }}
+        style={styles.itemPhoto}
+        resizeMode="cover"
+      />
+      <Text style={styles.itemText}>{item.text}</Text>
+    </View>
+  );
+};
+
 
 
 export default function Home({ navigation }) {
+  useEffect(() => {
+    let array = [];
+    firebase
+      .database()
+      .ref("BAIHAT/")
+      .on("value", function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+          var childData = childSnapshot.val();
+          SECTIONS[0].data.push({
+            key: childSnapshot.key,
+            text: childData.Ten,
+            uri: childData.Linkhava,
+          });
+        });
+        //setData(array);
+      });
+    }, [])
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Đây là trang chủ!!</Text>
-            <Button
-            title="Logout"
-            onPress={() =>
-                navigation.navigate('Login')
-            }
-            />
-        </View>
+      <View style={styles.container}>
+      <View style={styles.header}>  
+      <TextInput
+            style={styles.input}
+            // onChangeText={(text) => {
+            //   setPass(text);
+            // }}
+            // value={number}
+            placeholder="search"
+            keyboardType="numeric"
+          /> 
+      </View>
+      <StatusBar style="light" />
+      <SafeAreaView style={{ flex: 1}}>
+        <SectionList
+          contentContainerStyle={{ paddingHorizontal: 10 }}
+          stickySectionHeadersEnabled={false}
+          sections={SECTIONS}
+          renderSectionHeader={({ section }) => (
+            <>
+              <Text style={styles.sectionHeader}>{section.title}</Text>
+              {/* {section.horizontal ? ( */}
+                <FlatList
+                  horizontal
+                  data={section.data}
+                  renderItem={({ item }) => <ListItem item={item} />}
+                  showsHorizontalScrollIndicator={false}
+                />
+              {/*  ) : null} */}
+            </>
+          )}
+          renderItem={({ item, section }) => {
+            // if (section.horizontal) {
+            //   return null;
+            // }
+            // return <ListItem item={item} />;
+            return null;
+          }}
+        />
+      </SafeAreaView>
+    </View>
       );
 }
+
+let SECTIONS = [
+  {
+    title: 'Made for you',
+    horizontal: true,
+    data: [
+      {
+        key: '1',
+        text: 'Item text 1',
+        uri: 'https://picsum.photos/id/1/200',
+      },
+      {
+        key: '2',
+        text: 'Item text 2',
+        uri: 'https://picsum.photos/id/10/200',
+      },
+
+      {
+        key: '3',
+        text: 'Item text 3',
+        uri: 'https://picsum.photos/id/1002/200',
+      },
+      {
+        key: '4',
+        text: 'Item text 4',
+        uri: 'https://picsum.photos/id/1006/200',
+      },
+      {
+        key: '5',
+        text: 'Item text 5',
+        uri: 'https://picsum.photos/id/1008/200',
+      },
+    ],
+  },
+  {
+    title: 'Punk and hardcore',
+    data: [
+      {
+        key: '1',
+        text: 'Item text 1',
+        uri: 'https://picsum.photos/id/1011/200',
+      },
+      {
+        key: '2',
+        text: 'Item text 2',
+        uri: 'https://picsum.photos/id/1012/200',
+      },
+
+      {
+        key: '3',
+        text: 'Item text 3',
+        uri: 'https://picsum.photos/id/1013/200',
+      },
+      {
+        key: '4',
+        text: 'Item text 4',
+        uri: 'https://picsum.photos/id/1015/200',
+      },
+      {
+        key: '5',
+        text: 'Item text 5',
+        uri: 'https://picsum.photos/id/1016/200',
+      },
+    ],
+  },
+  {
+    title: 'Based on your recent listening',
+    data: [
+      {
+        key: '1',
+        text: 'Item text 1',
+        uri: 'https://picsum.photos/id/1020/200',
+      },
+      {
+        key: '2',
+        text: 'Item text 2',
+        uri: 'https://picsum.photos/id/1024/200',
+      },
+
+      {
+        key: '3',
+        text: 'Item text 3',
+        uri: 'https://picsum.photos/id/1027/200',
+      },
+      {
+        key: '4',
+        text: 'Item text 4',
+        uri: 'https://picsum.photos/id/1035/200',
+      },
+      {
+        key: '5',
+        text: 'Item text 5',
+        uri: 'https://picsum.photos/id/1038/200',
+      },
+    ],
+  },
+];
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121212',
+  },
+  sectionHeader: {
+    fontWeight: '800',
+    fontSize: 18,
+    color: '#f4f4f4',
+    marginTop: 20,
+    marginBottom: 5,
+  },
+  item: {
+    margin: 10,
+  },
+  itemPhoto: {
+    width: 200,
+    height: 200,
+  },
+  itemText: {
+    color: 'rgba(255, 255, 255, 0.5)',
+    marginTop: 5,
+  },
+  header: {
+    flex: 0.15,
+    marginTop: 10,
+    //position: 'absolute',
+  },
+  input: {
+    flex: 1,
+    marginVertical: 15,
+   // marginHorizontal: 18,
+    borderWidth: 2,
+    backgroundColor: "#F4ECD0",
+    borderColor: "#A88A64",
+    borderRadius: 7,
+    padding: 10,
+  },
+});
--- Content/screens/Signup.js
+++ Content/screens/Signup.js
@@ -1,20 +1,271 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  onChangeNumber,
+  number,
+  Button,
+  ImageBackground,
+  StyleSheet,
+  Image,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import background from "../assets/icon/background.png";
+import Logo from "../assets/icon/logo.png";
+import Checkbox from "expo-checkbox";
+import firebase from "firebase/compat/app";
+import ConnectFirebase from "../config";
+import "firebase/compat/auth";
+
+import "firebase/compat/database";
+import "firebase/compat/firestore";
 
 export default function Signup({ navigation }) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Đây là trang đăng ký!</Text>
+  const [username, setUsername] = useState(null);
+  const [name, setName] = useState(null);
+  const [password, setPass] = useState([]);
+  const [conpass, setConPass] = useState([]);
+  const [isChecked, setChecked] = useState(false);
+
+  function add_MEMBER(user, pass, conpass, ten) {
+    if (pass!=conpass){
+      alert("Nhập lại mật khẩu không đúng");
+    }
+    else {
+     
+    ConnectFirebase();
+    let array = [];
+    firebase
+      .database()
+      .ref("MEMBER/")
+      .on("value", function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+          var childData = childSnapshot.val();
+          array.push({
+            id: childSnapshot.key,
+            Username: childData.Username,
+            Password: childData.Password,
+          });
+        });
+        //setData(array);
+      });
+
+      firebase
+      .database()
+      .ref("BAIHAT/")
+      .push()
+      .set(
+        {
+          Ten: "Still with you",
+          Casi: "Jungkook",
+          Linkbaihat: "../assets/music/2.mp3",
+          Linkhava: "../assets/img/2.png",
+          Tag: "...",
+          Luotthich: 0,
+          Luotnghe:0,
+          Luottai:0
+        });
+
+    let i = 0;
+    while (i < array.length) {
+      if (array[i].Username == user ) {
+        alert("Tên tài khoản đã tồn tại");
+        break;
+      } else {
+        i = i + 1;
+      }
+      console.log(i);
+    }
+    if (i >= array.length) {
+      firebase
+      .database()
+      .ref("MEMBER/")
+      .push()
+      .set(
+        {
+          Username: username,
+          Password: password,
+          Hoten: ten,
+          Email: "lttt@gmail.com",
+        },
+        function (error) {
+          if (error) {
+            console.log("Lỗi");
+          } else {
+            console.log("Thành công");
+          }
+        }
+      );
+      alert("Thành công");
+    }
+     
+    }
+  }
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={background}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <Text style={styles.header}>ĐĂNG KÝ</Text>
+        <View style={styles.inputlogin}>
+          <Text style={styles.text}>Tài khoản</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => {
+              setUsername(text);
+            }}
+            value={username}
+            placeholder="Nhập tên tài khoản"
+            keyboardType="numeric"
+          />
+          <Text style={styles.text}>Họ và tên</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => {
+              setName(text);
+            }}
+            value={name}
+            placeholder="Nhập họ và tên"
+            keyboardType="numeric"
+          />
+          <Text style={styles.text}>Mật khẩu</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => {
+              setPass(text);
+            }}
+            value={password}
+            placeholder="Nhập mật khẩu"
+            keyboardType="numeric"
+          />
+          <Text style={styles.text}>Nhập lại mật khẩu</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => {
+              setConPass(text);
+            }}
+            value={conpass}
+            placeholder="Nhập lại mật khẩu"
+            keyboardType="numeric"
+          />
+        </View>
+
+        <View style={styles.row}>
+          
+        </View>
+        <View style={styles.btlogin}>
           <Button
             title="Đăng Ký"
+            color="#7B6242"
+            onPress={() => {
+                add_MEMBER(username, password,conpass,name);
+              }}
           />
-          <Button
-            title="Back"
-            onPress={() =>
-              navigation.navigate('Login')
-            }
-            />
         </View>
-        
-      );
+        <Text
+          style={styles.labelFGPW}
+          onPress={() => navigation.navigate("Login")}
+        >
+          Bạn đã có tài khoản?
+        </Text>
+        {/* <Text style={styles.text}>Đây là trang login!</Text> 
+          <Button
+            title="Đăng Nhập"
+            onPress={() =>
+              navigation.navigate('Botnavi')
+            }
+          />
+         <Button 
+            title="Chưa có tài khoản?"
+            onPress={() =>
+              navigation.navigate('Signup')
+            }
+            style={styles.button}
+          /> */}
+      </ImageBackground>
+    </View>
+  );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  inputlogin: {
+    flex: 1,
+    width: 380,
+  },
+  row: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    width: 380,
+  },
+  header: {
+    flex: 0.25,
+    color: "#A88A64",
+    fontSize: 40,
+    marginTop: 100,
+  },
+  image: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    flex: 1,
+    width: 380,
+  },
+  text: {
+    flex: 1,
+    fontStyle: "normal",
+    fontWeight: "bold",
+    alignSelf: "flex-start",
+    fontSize: 15,
+    color: "#7B6242",
+    marginLeft: 20,
+  },
+  input: {
+    flex: 1,
+    marginVertical: 15,
+    marginHorizontal: 18,
+    borderWidth: 2,
+    backgroundColor: "#F4ECD0",
+    borderColor: "#A88A64",
+    borderRadius: 7,
+    padding: 10,
+  },
+  checkbox: {
+    marginLeft: 18,
+    height: 15,
+    width: 15,
+    backgroundColor: "#F4ECD0",
+    borderColor: "#A88A64",
+    borderRadius: 5,
+    color: "#A88A64",
+  },
+  labelCheckBox: {
+    alignSelf: "flex-start",
+    marginLeft: 5,
+    fontSize: 10,
+    color: "#7B6242",
+  },
+  labelFGPW: {
+    alignSelf: "center",
+    marginBottom: 15,
+    fontSize: 15,
+    fontStyle: "italic",
+    color: "#7B6242",
+  },
+  button: {
+    flex: 1,
+  },
+  btlogin: {
+    flex: 0.5,
+    width: 150,
+    height: 15,
+    marginTop: 10,
+    color: "#7B6242",
+  },
+});
