e475e21cf26a9a70ad0bb8710720da6fde08127c
huuthang196 <19522200@m.uit.edu.vn>
2022-06-16 20:49:29 +0700

    screen chart

--- Content/screens/Chart.js
+++ Content/screens/Chart.js
@@ -1,4 +1,5 @@
-import { View, Text, ScrollView, ImageBackground, TouchableHighlight, TouchableOpacity, Button, StyleSheet, Image } from 'react-native'
+import { View,FlatList, Text, ScrollView, ImageBackground,
+   TouchableHighlight, TouchableOpacity, Button, StyleSheet, Image } from 'react-native'
 import React, { Component, useState } from 'react';
 import background from "../assets/icon/background.png";
 import { Avatar, Title, Caption, TouchableRipple } from 'react-native-paper';
@@ -7,17 +8,47 @@ import { Avatar, Title, Caption, TouchableRipple } from 'react-native-paper';
 
 
 export default function Chart({ navigation }) {
+// tạo mẫu để chạy giao diện thui nhe
   const [song, setSong] = useState([
-    {name: 'Still with you', key: '1'},
-    {name: 'Heyahe', key: '2'},
-    {name: 'Instagram', key: '3'},
-    {name: 'Mưa hồng', key: '4'},
-    {name: 'Long long ago', key: '5'},
-    {name: 'Mưa hồng', key: '6'},
-    {name: 'Thương', key: '7'},
+    {name: 'Still with you',singer: 'Jungkook', image: '../assets/img/2.png', key: '1'},
+    {name: 'Heyahe',singer: 'Jung Je-Won',image: '../assets/img/3.png', key: '2'},
+    {name: 'Instagram',singer: 'Dean',image: '../assets/img/4.png', key: '3'},
+    {name: 'Mưa hồng',singer: 'Khánh Ly',image: '../assets/img/5.png', key: '4'},
+    {name: 'Long long ago',singer: 'Unknow',image: '../assets/img/6.png', key: '5'},
+    {name: 'How you like that',singer: 'Lê Cát Trọng Lý',image: '../assets/img/7.png', key: '6'},
+    {name: 'Thương',singer: 'Blackpink',image: '../assets/img/8.png', key: '7'},
   ]);
   return (
-    <Text >Still with you </Text>
+    
+    <View style={StyleSheet.container}>
+      <View style={{ padding: 10, width: '100%', backgroundColor: '#7B6242', height: 100 }}>
+        <View style={styles.header}>
+          <Image source={require('../assets/icon/chart.png')}
+                style={{ width: 30, height: 30, paddingRight: 20 }}></Image>
+          <Title style={{ fontSize: 20, color: '#F4ECD0', fontWeight: 'bold', marginLeft: 20 }}>Top bài hát được nghe nhiều nhất</Title>
+        </View>
+      </View>
+
+      <Text style={styles.sectionTitle}>#RChart</Text>
+
+      <FlatList 
+        data={song}
+        renderItem={({item}) => (
+          <TouchableOpacity style={styles.item}>
+            <View style={styles.itemLeft}>
+              <Image source = {item.image} style={styles.avtSong}></Image>
+              <View style={styles.itemTitle}>
+                <Text style={styles.nameSong}>{item.name}</Text>
+                <Text style={styles.singer}>{item.singer}</Text>
+              </View>
+            </View>
+            <TouchableOpacity style={styles.circular}></TouchableOpacity>
+          </TouchableOpacity>
+          
+        )}
+      
+      />
+    </View>
     // <View>
     //   <ScrollView>
     //     <View style={{ padding: 10, width: '100%', backgroundColor: '#7B6242', height: 100 }}>
@@ -43,106 +74,96 @@ export default function Chart({ navigation }) {
     //           </View>
     //           <TouchableOpacity style={styles.circular}></TouchableOpacity>
     //         </TouchableOpacity>
-
-    //         <TouchableOpacity style={styles.items}>
-    //           <View style={styles.itemLeft}>
-    //             <View >
-    //               <Image source={require('../assets/img/1.png')} style={styles.avtSong}></Image>
-    //             </View>
-    //             <View style={styles.itemTitle}>
-    //               <Text style={styles.nameSong}>Heyahe </Text>
-    //               <Text style={styles.singer}>Jung Je-Won</Text>
-    //             </View>
-    //           </View>
-    //           <TouchableOpacity style={styles.circular}></TouchableOpacity>
-    //         </TouchableOpacity>
-
-    //         <TouchableOpacity style={styles.items}>
-    //           <View style={styles.itemLeft}>
-    //             <View >
-    //               <Image source={require('../assets/img/3.png')} style={styles.avtSong}></Image>
-    //             </View>
-    //             <View style={styles.itemTitle}>
-    //               <Text style={styles.nameSong}>Instagram</Text>
-    //               <Text style={styles.singer}>Dean</Text>
-    //             </View>
-    //           </View>
-    //           <TouchableOpacity style={styles.circular}></TouchableOpacity>
-    //         </TouchableOpacity>
-
-    //         <TouchableOpacity style={styles.items}>
-    //           <View style={styles.itemLeft}>
-    //             <View >
-    //               <Image source={require('../assets/img/4.png')} style={styles.avtSong}></Image>
-    //             </View>
-    //             <View style={styles.itemTitle}>
-    //               <Text style={styles.nameSong}>Mưa hồng</Text>
-    //               <Text style={styles.singer}>Khánh Ly</Text>
-    //             </View>
-    //           </View>
-    //           <TouchableOpacity style={styles.circular}></TouchableOpacity>
-    //         </TouchableOpacity>
-
-    //         <TouchableOpacity style={styles.items}>
-    //           <View style={styles.itemLeft}>
-    //             <View >
-    //               <Image source={require('../assets/img/5.png')} style={styles.avtSong}></Image>
-    //             </View>
-    //             <View style={styles.itemTitle}>
-    //               <Text style={styles.nameSong}>Long long ago</Text>
-    //               <Text style={styles.singer}>Unknow</Text>
-    //             </View>
-    //           </View>
-    //           <TouchableOpacity style={styles.circular}></TouchableOpacity>
-    //         </TouchableOpacity>
-
-    //         <TouchableOpacity style={styles.items}>
-    //           <View style={styles.itemLeft}>
-    //             <View >
-    //               <Image source={require('../assets/img/6.png')} style={styles.avtSong}></Image>
-    //             </View>
-    //             <View style={styles.itemTitle}>
-    //               <Text style={styles.nameSong}>Thương </Text>
-    //               <Text style={styles.singer}>Lê Cát Trọng Lý</Text>
-    //             </View>
-    //           </View>
-    //           <TouchableOpacity style={styles.circular}></TouchableOpacity>
-    //         </TouchableOpacity>
-
-    //         <TouchableOpacity style={styles.items}>
-    //           <View style={styles.itemLeft}>
-    //             <View >
-    //               <Image source={require('../assets/img/7.png')} style={styles.avtSong}></Image>
-    //             </View>
-    //             <View style={styles.itemTitle}>
-    //               <Text style={styles.nameSong}>How you like that </Text>
-    //               <Text style={styles.singer}>Blackpink</Text>
-    //             </View>
-    //           </View>
-    //           <TouchableOpacity style={styles.circular}></TouchableOpacity>
-    //         </TouchableOpacity>
-
-    //         <TouchableOpacity style={styles.items}>
-    //           <View style={styles.itemLeft}>
-    //             <View >
-    //               <Image source={require('../assets/img/9.png')} style={styles.avtSong}></Image>
-    //             </View>
-    //             <View style={styles.itemTitle}>
-    //               <Text style={styles.nameSong}>Still with you </Text>
-    //               <Text style={styles.singer}>Jungkook</Text>
-    //             </View>
-    //           </View>
-    //           <TouchableOpacity style={styles.circular}></TouchableOpacity>
-    //         </TouchableOpacity>
-    //       </View>
-
-    //     </View>
-
-    //   </ScrollView>
-    // </View>
-
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 40,
+    paddingHorizontal: 20
+  },
+  item: {
+    
+    padding: 5,
+    height: 70,
+    // width: 333,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    // fontSize: 20,
+    // fontWeight: 'bold',
+    marginHorizontal: 10,
+    marginLeft: 90,
+    marginBottom: 10,
+    marginTop: 20,
+    borderColor: '#7B6242',
+    borderWidth: 2, 
+  },
+  header: {
+    flex: 1,
+    flexDirection: 'row',
+    // paddingBottom: 10,
+    marginTop: 40,
+  },
+  sectionTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#7B6242',
+    paddingLeft: 20,
+  },
+  itemTitle: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  itemLeft: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+  },
+  nameSong: {
+    maxWidth: '80%',
+    fontSize: 20,
+    color: '#7B6242',
+    fontWeight: 'bold',
+    paddingLeft: 10,
+    height: 30,
+    marginBottom: 15,
+  },
+  avtSong: {
+    width: 55,
+    height: 55,
+    opacity: 0.4,
+    borderRadius: 5,
+    // marginRight: 5,
+    // paddingBottom: 15,
+    paddingHorizontal: 5,
+    marginHorizontal: 5
+  },
+  singer: {
+    fontWeight: 'bold',
+    width: '80%',
+    fontSize: 14,
+    paddingLeft: -60,
+    color: '#7B6242',
+    height: 20,
+    marginLeft: 10,
+    // paddingBottom: 5,
+    marginTop: -10,
+  },
+  circular: {
+    width: 12,
+    height: 12,
+    borderColor: '#7B6242',
+    borderWidth: 2,
+    borderRadius: 5,
+    marginLeft: 350,
+    marginBottom: 20,
+  },
+});
 // const styles = StyleSheet.create({
 //   container: {
 //     flex: 1,
@@ -181,7 +202,7 @@ export default function Chart({ navigation }) {
 //     marginBottom: 20,
 //     marginTop: 20,
 //     borderColor: '#7B6242',
-//     borderWidth: 2,
+//     borderWidth: 2, weight: 333,
 //   },
 //   itemLeft: {
 //     flex: 1,
